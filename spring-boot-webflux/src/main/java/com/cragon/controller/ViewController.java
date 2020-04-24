79d8f779a076c4a515a6588387a1a425996a857a
CragonLong <155190625@qq.com>
2020-04-24 16:31:36 +0800

    Spring boot中整合Webflux进行非阻塞调用

---
+++ spring-boot-webflux/src/main/java/com/cragon/controller/ViewController.java
@@ -0,0 +1,12 @@
+package com.cragon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    @GetMapping("flux")
+    public String flux() {
+        return "flux";
+    }
+}
---
+++ spring-boot-webflux/src/test/java/com/cragon/MonoFluxTest.java
@@ -0,0 +1,48 @@
+package com.cragon;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.TimeUnit;
+
+public class MonoFluxTest {
+    public static void main(String[] args) {
+        Subscriber<Integer> subscriber = new Subscriber<Integer>() {
+            private Subscription subscription;
+
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                this.subscription = subscription;
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                System.out.println("接收到数据：" + integer);
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+                this.subscription.cancel();
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("处理结束！");
+            }
+        };
+
+        String[] strs = {"1", "2", "3"};
+//        Flux.fromArray(strs).map(Integer::parseInt).subscribe(subscriber);
+        Mono.fromSupplier(() -> 1).map(s -> s + 1).subscribe(subscriber);
+    }
+}
